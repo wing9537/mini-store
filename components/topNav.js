c0b89bb2a7ab17dddb6282a8c1ad6ce8f54261a6
wing9537 <tommywing37@gmail.com>
2022-05-28 15:45:55 +0800

    add docs

--- components/topNav.js
+++ components/topNav.js
@@ -1,17 +1,7 @@
 import React, { useState } from "react";
 import { useRouter } from "next/router";
 import useTranslation from "../hook/useTranslation";
-import {
-  Container,
-  AppBar,
-  Toolbar,
-  Menu,
-  MenuItem,
-  Box,
-  Typography,
-  Button,
-  IconButton,
-} from "@mui/material";
+import { Container, AppBar, Toolbar, Menu, MenuItem, Box, Typography, Button, IconButton } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import LangIcon from "@mui/icons-material/Language";
 import messages from "../locales/topNav.json";
@@ -46,7 +36,7 @@ function TopNav() {
   const [anchorElLang, setAnchorElLang] = useState(null);
   const { pathname, asPath, push } = useRouter();
   const { t, obj } = useTranslation(messages);
-  const pages = Object.entries(obj("pages"));
+  const pages = Object.entries(obj("pages") || {});
   const lauguages = { en: "EN", tc: "繁", sc: "簡" };
 
   const handleOpenNavMenu = (e) => {
@@ -76,56 +66,25 @@ function TopNav() {
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ mr: 2, display: "flex" }}
-          >
+          <Typography variant="h6" noWrap component="div" sx={{ mr: 2, display: "flex" }}>
             {t("logo", { dev: "Tommy Kwok" })}
           </Typography>
           <Box sx={{ display: { xs: "none", md: "flex" } }}>
             {pages.map(([key, page]) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: "white", display: "block" }}
-              >
+              <Button key={page} onClick={handleCloseNavMenu} sx={{ my: 2, color: "white", display: "block" }}>
                 {page}
               </Button>
             ))}
           </Box>
           <Box sx={{ marginLeft: "auto" }}>
-            <IconButton
-              size="large"
-              onClick={handleOpenLangMenu}
-              color="inherit"
-            >
+            <IconButton size="large" onClick={handleOpenLangMenu} color="inherit">
               <LangIcon />
             </IconButton>
-            <MenuList
-              id="menu-lang"
-              openEl={anchorElLang}
-              onClose={handleCloseLangMenu}
-              onOpen={switchLang}
-              items={Object.entries(lauguages)}
-            />
-            <IconButton
-              size="large"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-              sx={{ display: { md: "none" } }}
-            >
+            <MenuList id="menu-lang" openEl={anchorElLang} onClose={handleCloseLangMenu} onOpen={switchLang} items={Object.entries(lauguages)} />
+            <IconButton size="large" onClick={handleOpenNavMenu} color="inherit" sx={{ display: { md: "none" } }}>
               <MenuIcon />
             </IconButton>
-            <MenuList
-              id="menu-appbar"
-              openEl={anchorElNav}
-              onClose={handleCloseNavMenu}
-              onOpen={switchPage}
-              sx={{ display: { md: "none" } }}
-              items={pages}
-            />
+            <MenuList id="menu-appbar" openEl={anchorElNav} onClose={handleCloseNavMenu} onOpen={switchPage} sx={{ display: { md: "none" } }} items={pages} />
           </Box>
         </Toolbar>
       </Container>
