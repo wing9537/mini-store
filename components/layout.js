bac9bbff0abd4fad6ef5ec575ae89829842ec69b
Tommy Kwok <37375762+wing9537@users.noreply.github.com>
2022-06-19 22:20:05 +0800

    add github link

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,16 @@
 import Head from "next/head";
 import TopNav from "./topNav";
-import { Container } from "@mui/material";
+import { Box, Container } from "@mui/material";
 
 function Footer() {
-  return <footer>Powered by Tommy Kwok</footer>;
+  return (
+    <footer>
+      <div>Powered by Tommy Kwok</div>
+      <a href="https://github.com/wing9537/mini-store/" target="_blank">
+        GitHub:&ensp;<u>https://github.com/wing9537/mini-store/</u>
+      </a>
+    </footer>
+  );
 }
 
 function Layout({ children }) {
